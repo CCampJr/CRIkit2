7664fedb1b215671cfdbca368af7ededdaed16d5
Charles H. Camp Jr <ccampjr@gmail.com>
2016-04-12 16:09:01 -0400

    x and y and x_rep and y_rep set and get implemented

--- crikit/data/hsi.py
+++ crikit/data/hsi.py
@@ -15,8 +15,9 @@ if __name__ == '__main__':  # pragma: no cover
     _sys.path.append(_os.path.abspath('.'))
 from crikit.data.frequency import Frequency as _Frequency
 from crikit.data.spectrum import Spectrum as _Spectrum
+from crikit.data.replicate import Replicate as _Replicate
 
-__all__ = ['Spectra']
+__all__ = ['Hsi']
 
 class Hsi(_Spectrum):
     """
@@ -56,6 +57,9 @@ class Hsi(_Spectrum):
         Size of data's frequency axis. Note: this matches the size of data and \
         does NOT check the size of freq.freq_vec.
 
+    shape : tuple, read-only
+        Shape of data
+
     Methods
     -------
 
@@ -84,6 +88,15 @@ class Hsi(_Spectrum):
             self.freq = freq
         else:
             self.freq = _Frequency()
+
+        self._x_rep = _Replicate(data=x)
+        self._y_rep = _Replicate(data=y)
+
+        if x is None and x_rep is not None:
+            self.x_rep = x_rep
+        if y is None and y_rep is not None:
+            self.y_rep = y_rep
+
         if label is not None:
             self.label = label
         if units is not None:
@@ -91,7 +104,44 @@ class Hsi(_Spectrum):
         if meta is not None:
             self.meta = meta
 
+    @property
+    def x_rep(self):
+        return self._x_rep
 
+    @x_rep.setter
+    def x_rep(self, value):
+        if isinstance(value, _Replicate):
+            self._x_rep = value
+        elif isinstance(value, _np.ndarray):
+            self._x_rep.data = value
+
+    @property
+    def y_rep(self):
+        return self._y_rep
+
+    @property
+    def x(self):
+        return self._x_rep.data
+
+    @x.setter
+    def x(self, value):
+        self._x_rep.data = value
+
+    @property
+    def y(self):
+        return self._y_rep.data
+
+    @y.setter
+    def y(self, value):
+        self._y_rep.data = value
+
+
+    @y_rep.setter
+    def y_rep(self, value):
+        if isinstance(value, _Replicate):
+            self._y_rep = value
+        elif isinstance(value, _np.ndarray):
+            self._y_rep.data = value
 
     @property
     def data(self):
@@ -100,32 +150,25 @@ class Hsi(_Spectrum):
     @data.setter
     def data(self, value):
         if isinstance(value, _np.ndarray):
-            if value.ndim == 1:
-                print('Spectra: converting data input from 1D to 2D ndarray')
-                self._data = value[None,:]
-            elif value.ndim == 2:
+            if value.ndim == 3:
                 self._data = value
             else:
-                print('Spectra: converting data input from {}D to 2D ndarray'.format(value.ndim))
-                f_sh = value.shape[-1]
-                self._data = value.reshape((-1, f_sh))
+                raise TypeError('data must be 3D')
         else:
-               raise TypeError('data must be a ndarray')
+           raise TypeError('data must be a 3D ndarray')
 
     @property
-    def n_pix(self):
-        return self._data.shape[0]
-
-    @property
-    def reps(self):
-        raise NotImplementedError
+    def shape(self):
+        return self._data.shape
 
 
 if __name__ == '__main__': # pragma: no cover
-    sp = Spectra()
-    print(sp.__dict__)
-    print('Subclass? : {}'.format(issubclass(Spectra,_Spectrum)))
-    print('Instance of Spectra? : {}'.format(isinstance(sp,Spectra)))
-    print('Instance of Spectrum? : {}'.format(isinstance(sp,_Spectrum)))
-    print('Type(sp) == Spectrum? : {}'.format(type(sp)==_Spectrum))
-    print('Type(sp) == Spectra? : {}'.format(type(sp)==Spectra))
+
+    x = _np.linspace(0,100,10)
+    y = _np.linspace(0,100,10)
+    freq = _np.arange(20)
+    data = _np.random.rand(10,10,20)
+
+
+    hs = Hsi(data=data, freq=freq, x=x, y=y)
+    print(hs.shape)
