5ca10d1a4b94c72f6992b61972191bc0748bb06e
Charles H. Camp Jr <ccampjr@gmail.com>
2016-06-17 18:05:54 -0400

    From function to class

--- crikit/preprocess/standardize.py
+++ crikit/preprocess/standardize.py
@@ -15,14 +15,13 @@ if __name__ == '__main__':  # pragma: no cover
     _sys.path.append(_os.path.abspath('../../'))
 
 import timeit as _timeit
-
+import copy as _copy
 import numpy as _np
 
 from crikit.preprocess.algorithms.anscombe import (gen_anscombe_forward as ansc,
                                                    gen_anscombe_inverse_exact_unbiased as inv_ansc)
 
-def anscombe(data, gauss_std, gauss_mean=0.0, poisson_multi=1.0,
-             rng=None, overwrite=True, **kwargs):
+class Anscombe:
     """
     Implement the generalized forward Anscombe transformation.
 
@@ -84,29 +83,83 @@ def anscombe(data, gauss_std, gauss_mean=0.0, poisson_multi=1.0,
         spectroscopy: Correcting errors in phase retrieval," Journal of Raman \
         Spectroscopy 47, 408-415 (2016). arXiv:1507.06543.
     """
+    def __init__(self, gauss_std, gauss_mean=0.0, poisson_multi=1.0,
+             rng=None, **kwargs):
 
-    # Anscombe transform
-    if rng is None:
-        out = ansc(data, gauss_std=gauss_std,
-                   gauss_mean=gauss_mean,
-                   poisson_multi=poisson_multi)
-    else:
-        out = ansc(data[..., rng], gauss_std=gauss_std,
-                   gauss_mean=gauss_mean,
-                   poisson_multi=poisson_multi)
+        self.gauss_std = gauss_std
+        self.gauss_mean = gauss_mean
+        self.poisson_multi = poisson_multi
 
-    if overwrite:
-        data *= 0
         if rng is None:
-            data += out
+            self.rng = None
+        elif len(rng) == 2:
+            rng.sort()
+            self.rng = _np.arange(rng[0],rng[1])
         else:
-            data[..., rng] = out
-        return None
-    else:
-        return out
+            self.rng = rng
 
-def anscombe_inverse(data, gauss_std, gauss_mean=0.0, poisson_multi=1.0,
-                     rng=None, overwrite=True, **kwargs):
+    def _calc(self, data, ret_obj):
+        # Anscombe transform
+        if self.rng is None:
+            self.rng = _np.arange(data.shape[-1])
+            out = ansc(data, gauss_std=self.gauss_std,
+                       gauss_mean=self.gauss_mean,
+                       poisson_multi=self.poisson_multi)
+        else:
+            out = ansc(data[..., self.rng], gauss_std=self.gauss_std,
+                       gauss_mean=self.gauss_mean,
+                       poisson_multi=self.poisson_multi)
+
+        try:
+            ret_obj *= 0
+            ret_obj[..., self.rng] = out
+        except:
+            return False
+        else:
+            return True
+
+    def transform(self, data):
+        """
+        Generalized Anscombe transform (Overwrite data).
+
+        Parameters
+        ----------
+        data : ndarray
+            Input data.
+
+        Returns
+        -------
+        bool
+            Returns the success state (True=success)
+
+        """
+        success = self._calc(data, ret_obj=data)
+        return success
+
+    def calculate(self, data):
+        """
+        Generalized Anscombe transform (Return copy).
+
+        Parameters
+        ----------
+        data : ndarray
+            Input data.
+
+        Returns
+        -------
+        ndarray
+            Returns Anscombe-transformed data (or None if fails)
+
+        """
+        data_copy = _copy.deepcopy(data)
+        success = self._calc(data, ret_obj=data_copy)
+        if success:
+            return data_copy
+        else:
+            return None
+
+
+class AnscombeInverse:
     """
     Applies an exact, unbiased inverse of the generalized Anscombe \
     variance-stabilizing transformation assuming a mixed Poisson-Gaussian \
@@ -164,26 +217,79 @@ def anscombe_inverse(data, gauss_std, gauss_mean=0.0, poisson_multi=1.0,
         Spectroscopy 47, 408-415 (2016). arXiv:1507.06543.
     """
 
+    def __init__(self, gauss_std, gauss_mean=0.0, poisson_multi=1.0, rng=None):
+        self.gauss_std = gauss_std
+        self.gauss_mean = gauss_mean
+        self.poisson_multi = poisson_multi
 
-    # Inverse Anscombe transform
-    if rng is None:
-        out = inv_ansc(data, gauss_std=gauss_std,
-                   gauss_mean=gauss_mean,
-                   poisson_multi=poisson_multi)
-    else:
-        out = inv_ansc(data[..., rng], gauss_std=gauss_std,
-                   gauss_mean=gauss_mean,
-                   poisson_multi=poisson_multi)
-
-    if overwrite:
-        data *= 0
         if rng is None:
-            data += out
+            self.rng = None
+        elif len(rng) == 2:
+            rng.sort()
+            self.rng = _np.arange(rng[0],rng[1])
         else:
-            data[..., rng] = out
-        return None
-    else:
-        return out
+            self.rng = rng
+
+    def _calc(self, data, ret_obj):
+
+        # Inverse Anscombe transform
+        if self.rng is None:
+            self.rng = _np.arange(data.shape[-1])
+            out = inv_ansc(data, gauss_std=self.gauss_std,
+                       gauss_mean=self.gauss_mean,
+                       poisson_multi=self.poisson_multi)
+        else:
+            out = inv_ansc(data[..., self.rng], gauss_std=self.gauss_std,
+                       gauss_mean=self.gauss_mean,
+                       poisson_multi=self.poisson_multi)
+
+        try:
+            ret_obj *= 0
+            ret_obj[..., self.rng] = out
+        except:
+            return False
+        else:
+            return True
+
+    def transform(self, data):
+        """
+        Generalized Inverse Anscombe transform (Overwrite data).
+
+        Parameters
+        ----------
+        data : ndarray
+            Input data.
+
+        Returns
+        -------
+        bool
+            Returns the success state (True=success)
+
+        """
+        success = self._calc(data, ret_obj=data)
+        return success
+
+    def calculate(self, data):
+        """
+        Generalized Inverse Anscombe transform (Return copy).
+
+        Parameters
+        ----------
+        data : ndarray
+            Input data.
+
+        Returns
+        -------
+        ndarray
+            Returns Anscombe-transformed data (or None if fails)
+
+        """
+        data_copy = _copy.deepcopy(data)
+        success = self._calc(data, ret_obj=data_copy)
+        if success:
+            return data_copy
+        else:
+            return None
 
 if __name__ == '__main__': # pragma: no cover
 
@@ -201,24 +307,68 @@ if __name__ == '__main__': # pragma: no cover
 
     import matplotlib.pyplot as _plt
 
+    anscombe = Anscombe(gauss_std=stddev, gauss_mean=0, poisson_multi=gain)
+
     sig2 = _Spectrum(sig)
-    out = anscombe(sig2.data, gauss_std=stddev, poisson_multi=gain, overwrite=False)
-    _plt.plot(out, label='No overwrite')
-    anscombe(sig2.data, gauss_std=stddev, poisson_multi=gain, overwrite=True)
-    _plt.plot(sig2.data, label='Overwrite')
+
+    out = anscombe.calculate(sig2.data)
+    _plt.subplot(211)
+    _plt.plot(sig2.data, label='Data')
+    _plt.title('Untransformed Space')
     _plt.legend(loc='best')
+
+    _plt.subplot(212)
+    _plt.plot(out, label='Calculate')
+
+    out2 = anscombe.transform(sig2.data)
+    _plt.plot(sig2.data, label='Transform')
+    _plt.legend(loc='best')
+    _plt.title('Anscombe Transform')
     _plt.show()
 
-    print(_np.allclose(out, sig2.data))
+    print('Transform and Calculate Equivalent: {}'.format(_np.allclose(out,
+          sig2.data)))
 
-    # Recalc sig
-    sig = 10e4*_np.exp(-(f-2000)**2/(500**2))
+    inverse_anscombe = AnscombeInverse(gauss_std=stddev, gauss_mean=0,
+                                       poisson_multi=gain)
+
+    sig2 = _Spectrum(sig)
+    sig2_ansc = anscombe.calculate(sig2.data)
+
+    out = inverse_anscombe.calculate(sig2_ansc)
+    _plt.subplot(211)
+    _plt.plot(sig2.data, label='Data')
+    _plt.plot(out, label='Calculate')
+    _plt.title('Untransformed Space/Inverse Anscombe')
+
+    _plt.legend(loc='best')
+
+    _plt.subplot(212)
+    _plt.plot(sig2_ansc, label='Anscombe')
+
+
+    _plt.legend(loc='best')
+    _plt.title('Anscombe Transform')
+    _plt.show()
+
+#    print('Data and Inverse of Anscombe Close: {}'.format(_np.allclose(out,
+#          sig2.data)))
 
     _plt.figure()
-    sig_ansc = anscombe(sig.data, gauss_std=stddev, poisson_multi=gain, overwrite=False)
-    out = anscombe_inverse(sig_ansc, gauss_std=stddev, poisson_multi=gain, overwrite=False)
-    _plt.plot(sig)
-    _plt.plot(out)
-    anscombe_inverse(sig_ansc, gauss_std=stddev, poisson_multi=gain, overwrite=True)
-    _plt.plot(sig_ansc)
+    _plt.plot(out-sig2.data)
+    _plt.title('Inverse Anscombe - Original Data')
     _plt.show()
+#
+#    print((out-sig2.data)[0])
+
+#    # Recalc sig
+#    sig = 10e4*_np.exp(-(f-2000)**2/(500**2))
+#
+#    _plt.figure()
+#    sig_ansc = anscombe(sig.data, gauss_std=stddev, poisson_multi=gain, overwrite=False)
+#    out = anscombe_inverse(sig_ansc, gauss_std=stddev, poisson_multi=gain, overwrite=False)
+#    _plt.plot(sig)
+#    _plt.plot(out)
+#    anscombe_inverse(sig_ansc, gauss_std=stddev, poisson_multi=gain, overwrite=True)
+#    _plt.plot(sig_ansc)
+#    _plt.show()
