ec423e35b7b2bf93bd869612877cf73f08703ddf
Charles H. Camp Jr <ccampjr@gmail.com>
2016-07-21 18:42:26 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 21 01:11:04 2016
+
+@author: chc
+"""
+
+from setuptools import setup, find_packages
+
+setup(name='crikit2',
+      version = '0.1',
+      description = 'Hyperspectral imaging (HSI) processing and analysis \
+      platform (user interface, UI)',
+      url = 'https://github.com/CCampJr/crikit2',
+      author = 'Charles H. Camp Jr.',
+      author_email = 'charles.camp@nist.gov',
+      license = 'NONLICENSE',
+      packages = find_packages(),
+      entry_points={
+          'gui_scripts': (
+              'crikit2-start = crikit.__main__:main')},
+      zip_safe = False,
+      include_package_data = True,
+      install_requires=['numpy','matplotlib','yapsy','sciplot-pyqt>=0.1.1'],
+      classifiers=['Development Status :: 2 - Pre-Alpha',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: Science/Research',
+                   'Operating System :: OS Independent',
+                   'Environment :: X11 Applications :: Qt',
+                   'Programming Language :: Python :: 3 :: Only',
+                   'Programming Language :: Python :: 3.4',
+                   'Topic :: Scientific/Engineering :: Visualization'])
