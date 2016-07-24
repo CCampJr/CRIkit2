eff64aced8afa045e774260b4191d065aaaec0ac
Charles H. Camp Jr <ccampjr@gmail.com>
2016-07-23 21:47:41 -0400

    Refactored and greatly reduced complexity and size

--- crikit/ui/dialog_subResidualOptions.py
+++ crikit/ui/dialog_subResidualOptions.py
@@ -29,7 +29,6 @@ class DialogSubResidualOptions(_QDialog):
     
     """
     RESIDUAL_FREQ = [-1500, -400]
-    SUB_RESIDUAL = True
 
     def __init__(self, parent = None):
         super(DialogSubResidualOptions, self).__init__(parent) ### EDIT ###
@@ -39,17 +38,7 @@ class DialogSubResidualOptions(_QDialog):
         self.ui.spinBoxMax.setValue(self.RESIDUAL_FREQ[0])
         self.ui.spinBoxMax.setValue(self.RESIDUAL_FREQ[1])
         
-        self.submain = False
-        self.subnrb = False
-        self.freq0 = self.RESIDUAL_FREQ[0]
-        self.freq1 = self.RESIDUAL_FREQ[1]
-
-        self.ui.checkBoxMain.stateChanged.connect(self.updateCheckBoxMain)
-        self.ui.checkBoxBG.stateChanged.connect(self.updateCheckBoxBG)
         
-        self.ui.spinBoxMin.editingFinished.connect(self.checkMinMax)
-        self.ui.spinBoxMax.editingFinished.connect(self.checkMinMax)
-
     @staticmethod
     def dialogSubResidualOptions(parent = None,
                                  imgloaded = False,
@@ -88,31 +77,16 @@ class DialogSubResidualOptions(_QDialog):
         result = dialog.exec_()
 
         if result == 1:
-            freq = [dialog.freq0, dialog.freq1]
-            freq.sort()
-            ret = {'submain' : dialog.submain, 'subnrb' : dialog.subnrb, 
-                   'subrange' : freq}
+            ret = {}
+            freq = [dialog.ui.spinBoxMin.value(),
+                    dialog.ui.spinBoxMax.value()]
+            ret['subrange'] = freq
+            ret['submain'] = dialog.ui.checkBoxMain.isChecked()
+            ret['subnrb'] = dialog.ui.checkBoxBG.isChecked()
+            
             return ret
         else:
             return None
-
-
-    def updateCheckBoxMain(self):
-        if self.ui.checkBoxMain.isChecked() == True:
-            self.submain = True
-        else:
-            self.submain = False
-            
-    def updateCheckBoxBG(self):
-        if self.ui.checkBoxBG.isChecked() == True:
-            self.subnrb = True
-        else:
-            self.subnrb = False
-
-    def checkMinMax(self):
-        self.freq0 = self.ui.spinBoxMin.value()
-        self.freq1 = self.ui.spinBoxMax.value()
-
 
 if __name__ == '__main__':
 
