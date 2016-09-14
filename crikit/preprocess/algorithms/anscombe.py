247eb09e30129b53c2408dfdf028c40aa0a02870
Charles H. Camp Jr <ccampjr@gmail.com>
2016-09-14 17:27:50 -0400

    Moved from numexpr to numpy for stability

--- crikit/preprocess/algorithms/anscombe.py
+++ crikit/preprocess/algorithms/anscombe.py
@@ -32,7 +32,7 @@ Citation Refs
 """
 
 import numpy as _np
-import numexpr as _ne
+#import numexpr as _ne
 import os as _os
 
 resource_dir = None
@@ -91,14 +91,14 @@ def gen_anscombe_forward(signal, gauss_std, gauss_mean = 0, poisson_multi = 1):
 
     SMALL_VAL = 1
 
-#    fsignal = 2/poisson_multi * _np.sqrt(_np.fmax(SMALL_VAL,poisson_multi*signal +
-#                                    (3/8)*poisson_multi**2 +
-#                                    gauss_std**2 -
-#                                    poisson_multi*gauss_mean))
-    fsignal = _ne.evaluate('2/poisson_multi * sqrt(where(poisson_multi*signal + (3/8)*poisson_multi**2 +\
-                            gauss_std**2 - poisson_multi*gauss_mean > SMALL_VAL,\
-                            poisson_multi*signal + (3/8)*poisson_multi**2 +\
-                            gauss_std**2 - poisson_multi*gauss_mean, SMALL_VAL))')
+    fsignal = 2/poisson_multi * _np.sqrt(_np.fmax(SMALL_VAL,poisson_multi*signal +
+                                    (3/8)*poisson_multi**2 +
+                                    gauss_std**2 -
+                                    poisson_multi*gauss_mean))
+#    fsignal = _ne.evaluate('2/poisson_multi * sqrt(where(poisson_multi*signal + (3/8)*poisson_multi**2 +\
+#                            gauss_std**2 - poisson_multi*gauss_mean > SMALL_VAL,\
+#                            poisson_multi*signal + (3/8)*poisson_multi**2 +\
+#                            gauss_std**2 - poisson_multi*gauss_mean, SMALL_VAL))')
     #fsignal = 2/poisson_multi * _np.sqrt(_np.fmax(SMALL_VAL,fsignal))
     return fsignal
 
@@ -165,11 +165,28 @@ def gen_anscombe_inverse_closed_form(fsignal, gauss_std, gauss_mean = 0,
 
     gauss_std = gauss_std/poisson_multi
 
-    signal = _ne.evaluate('poisson_multi*where((fsignal/2)**2 + 1/4*sqrt(3/2)*fsignal**-1 -\
-                    (11/8)*fsignal**-2 + 5/8*sqrt(3/2)*fsignal**-3 -\
-                    (1/8) - gauss_std**2 > SMALL_VAL, (fsignal/2)**2 + 1/4*sqrt(3/2)*fsignal**-1 -\
-                    (11/8)*fsignal**-2 + 5/8*sqrt(3/2)*fsignal**-3 -\
-                    (1/8) - gauss_std**2, SMALL_VAL) + gauss_mean')
+#    signal = _ne.evaluate('poisson_multi*where((fsignal/2)**2 + \
+#                    1/4*sqrt(3/2)*fsignal**-1 -\
+#                    (11/8)*fsignal**-2 + 5/8*sqrt(3/2)*fsignal**-3 -\
+#                    (1/8) - gauss_std**2 > SMALL_VAL, (fsignal/2)**2 + \
+#                    1/4*sqrt(3/2)*fsignal**-1 -\
+#                    (11/8)*fsignal**-2 + 5/8*sqrt(3/2)*fsignal**-3 -\
+#                    (1/8) - gauss_std**2, SMALL_VAL) + gauss_mean')
+    
+    signal = _np.fmax(SMALL_VAL, (fsignal/2)**2 + 1/4*_np.sqrt(3/2)*fsignal**-1 - 
+              (11/8)*fsignal**-2 + 5/8*_np.sqrt(3/2)*fsignal**-3 - 
+              (1/8) - gauss_std**2)
+    #signal[signal < SMALL_VAL] = SMALL_VAL
+    signal *= poisson_multi
+    
+#    signal = poisson_multi*where((fsignal/2)**2 + \
+#                                1/4*sqrt(3/2)*fsignal**-1 -\
+#                                (11/8)*fsignal**-2 + 5/8*sqrt(3/2)*fsignal**-3 -\
+#                                (1/8) - gauss_std**2 > SMALL_VAL, (fsignal/2)**2 + \
+#                                1/4*sqrt(3/2)*fsignal**-1 -\
+#                                (11/8)*fsignal**-2 + 5/8*sqrt(3/2)*fsignal**-3 -\
+#                                (1/8) - gauss_std**2, SMALL_VAL) \
+#                     + gauss_mean')
 
     return signal
 
@@ -350,8 +367,8 @@ def anscombe_inverse_exact_unbiased(fsignal):
     Efz = mat_dict['Efz']
     Ez = mat_dict['Ez']
 
-    #asymptotic = (fsignal/2)**2 - 1/8;  # asymptotically unbiased inverse [3]
-    asymptotic = _ne.evaluate('(fsignal/2)**2 - 1/8')  # asymptotically unbiased inverse [3]
+    asymptotic = (fsignal/2)**2 - 1/8;  # asymptotically unbiased inverse [3]
+#    asymptotic = _ne.evaluate('(fsignal/2)**2 - 1/8')  # asymptotically unbiased inverse [3]
 
     #start = time.process_time()
     signal = InterpolatedUnivariateSpline(Efz,Ez,k=1)(fsignal)   # exact unbiased inverse [1,2]
@@ -376,50 +393,52 @@ if __name__ == '__main__':  # pragma: no cover
     stddev = 20
     gain = 1
 
-    x = np.linspace(500,4000,1000)
+    x = np.linspace(500,4000,10000)
     y = 10e4*np.exp(-(x-2000)**2/(500**2))
     gnoise = stddev*np.random.randn(x.size)
     ygn = y + gnoise
     ymix = np.random.poisson(y) + gnoise
+#    ymix = _np.dot(_np.ones((1000,1)), ymix[None,:])
     ymix_ansc = gen_anscombe_forward(ymix, gauss_std=stddev, poisson_multi=gain)
     y_ansc = gen_anscombe_forward(y, gauss_std=stddev, poisson_multi=gain)
 
     y_inv_ansc = gen_anscombe_inverse_exact_unbiased(y_ansc, gauss_std=stddev,
                                                      poisson_multi=gain)
 
-    plt.subplot(211)
-    plt.plot(x,y, label='Signal')
-    plt.hold(True)
-    plt.plot(x,ymix, label='Mixed Noise Signal')
-    plt.title('Signal')
-    plt.legend(loc='best')
-
-    plt.subplot(212)
-    plt.plot(x,ymix-y, label='Mixed - Signal')
-    plt.title('Difference ($\sigma =$ {:.3f})'.format((ymix - y).std()))
-    plt.legend(loc='best')
-
-    plt.figure()
-    plt.subplot(211)
-    plt.plot(x,ymix_ansc, label='Anscombe Mixed Signal')
-    plt.plot(x,y_ansc, label='Anscombe Signal')
-    plt.title('Anscombe Transformed')
-    plt.legend(loc='best')
-
-    plt.subplot(212)
-    plt.plot(x,ymix_ansc - y_ansc, label='Ansc. Mixed - Ansc. Signal')
-    plt.title('Difference ($\sigma =$ {:.3f})'.format((ymix_ansc - y_ansc).std()))
-    plt.legend(loc='best')
-
-    plt.figure()
-    plt.subplot(211)
-    plt.plot(x,y_inv_ansc, label='Inv. Anscombe Signal')
-    plt.plot(x,y, label='Signal')
-    plt.title('Inverse Anscombe Transformed')
-    plt.legend(loc='best')
-
-    plt.subplot(212)
-    plt.plot(x,y_inv_ansc - ymix, label='Inv. Ansc. Signal - Mixed')
-    plt.title('Difference ($\sigma =$ {:.3f})'.format((y_inv_ansc - ymix).std()))
-    plt.legend(loc='best')
-    plt.show()
+    if ymix.ndim == 1:
+        plt.subplot(211)
+        plt.plot(x,y, label='Signal')
+        plt.hold(True)
+        plt.plot(x,ymix, label='Mixed Noise Signal')
+        plt.title('Signal')
+        plt.legend(loc='best')
+    
+        plt.subplot(212)
+        plt.plot(x,ymix-y, label='Mixed - Signal')
+        plt.title('Difference ($\sigma =$ {:.3f})'.format((ymix - y).std()))
+        plt.legend(loc='best')
+    
+        plt.figure()
+        plt.subplot(211)
+        plt.plot(x,ymix_ansc, label='Anscombe Mixed Signal')
+        plt.plot(x,y_ansc, label='Anscombe Signal')
+        plt.title('Anscombe Transformed')
+        plt.legend(loc='best')
+    
+        plt.subplot(212)
+        plt.plot(x,ymix_ansc - y_ansc, label='Ansc. Mixed - Ansc. Signal')
+        plt.title('Difference ($\sigma =$ {:.3f})'.format((ymix_ansc - y_ansc).std()))
+        plt.legend(loc='best')
+    
+        plt.figure()
+        plt.subplot(211)
+        plt.plot(x,y_inv_ansc, label='Inv. Anscombe Signal')
+        plt.plot(x,y, label='Signal')
+        plt.title('Inverse Anscombe Transformed')
+        plt.legend(loc='best')
+    
+        plt.subplot(212)
+        plt.plot(x,y_inv_ansc - ymix, label='Inv. Ansc. Signal - Mixed')
+        plt.title('Difference ($\sigma =$ {:.3f})'.format((y_inv_ansc - ymix).std()))
+        plt.legend(loc='best')
+        plt.show()
