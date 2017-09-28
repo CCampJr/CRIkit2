11b963acc9e3ea275cf289a8b91f94fec7d7a653
Charles H. Camp Jr <ccampjr@gmail.com>
2017-09-28 19:06:45 -0400

    Added frame and spacer for when items hiddem

---
+++ crikit/ui/qt_ColorModeSetting.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '.\ui_ColorModeSettings.ui'
+#
+# Created by: PyQt5 UI code generator 5.8.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(209, 264)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.frame = QtWidgets.QFrame(Form)
+        self.frame.setFrameShape(QtWidgets.QFrame.Box)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.frame)
+        self.verticalLayout_2.setContentsMargins(2, 2, 2, -1)
+        self.verticalLayout_2.setSpacing(2)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.verticalLayoutImgColors = QtWidgets.QVBoxLayout()
+        self.verticalLayoutImgColors.setContentsMargins(-1, -1, 10, -1)
+        self.verticalLayoutImgColors.setObjectName("verticalLayoutImgColors")
+        self.labelColorMode = QtWidgets.QLabel(self.frame)
+        self.labelColorMode.setObjectName("labelColorMode")
+        self.verticalLayoutImgColors.addWidget(self.labelColorMode)
+        self.comboBoxColorMode = QtWidgets.QComboBox(self.frame)
+        self.comboBoxColorMode.setMinimumSize(QtCore.QSize(0, 30))
+        self.comboBoxColorMode.setObjectName("comboBoxColorMode")
+        self.comboBoxColorMode.addItem("")
+        self.comboBoxColorMode.addItem("")
+        self.verticalLayoutImgColors.addWidget(self.comboBoxColorMode)
+        self.labelFGColor = QtWidgets.QLabel(self.frame)
+        self.labelFGColor.setObjectName("labelFGColor")
+        self.verticalLayoutImgColors.addWidget(self.labelFGColor)
+        self.comboBoxFGColor = QtWidgets.QComboBox(self.frame)
+        self.comboBoxFGColor.setMinimumSize(QtCore.QSize(0, 30))
+        self.comboBoxFGColor.setObjectName("comboBoxFGColor")
+        self.verticalLayoutImgColors.addWidget(self.comboBoxFGColor)
+        self.labelBGColor = QtWidgets.QLabel(self.frame)
+        self.labelBGColor.setObjectName("labelBGColor")
+        self.verticalLayoutImgColors.addWidget(self.labelBGColor)
+        self.comboBoxBGColor = QtWidgets.QComboBox(self.frame)
+        self.comboBoxBGColor.setMinimumSize(QtCore.QSize(120, 30))
+        self.comboBoxBGColor.setObjectName("comboBoxBGColor")
+        self.verticalLayoutImgColors.addWidget(self.comboBoxBGColor)
+        self.labelColormap = QtWidgets.QLabel(self.frame)
+        self.labelColormap.setObjectName("labelColormap")
+        self.verticalLayoutImgColors.addWidget(self.labelColormap)
+        self.comboBoxColormap = QtWidgets.QComboBox(self.frame)
+        self.comboBoxColormap.setMinimumSize(QtCore.QSize(0, 30))
+        self.comboBoxColormap.setObjectName("comboBoxColormap")
+        self.verticalLayoutImgColors.addWidget(self.comboBoxColormap)
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayoutImgColors.addItem(spacerItem)
+        self.verticalLayout_2.addLayout(self.verticalLayoutImgColors)
+        self.verticalLayout.addWidget(self.frame)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.labelColorMode.setText(_translate("Form", "Color Mode"))
+        self.comboBoxColorMode.setItemText(0, _translate("Form", "RGB"))
+        self.comboBoxColorMode.setItemText(1, _translate("Form", "Colormap"))
+        self.labelFGColor.setText(_translate("Form", "Image Color"))
+        self.labelBGColor.setText(_translate("Form", "Background Color"))
+        self.labelColormap.setText(_translate("Form", "Colormap"))
+
