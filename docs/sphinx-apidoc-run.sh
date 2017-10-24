f96b744ed905c9237b4f3760a812361ca8a5f6a1
Charles H. Camp Jr <ccampjr@gmail.com>
2017-10-23 20:29:00 -0400

    Updated sphinx-apidoc command

--- docs/sphinx-apidoc-run.sh
+++ docs/sphinx-apidoc-run.sh
@@ -1,2 +1,7 @@
-sphinx-apidoc -o ./source/ ../crikit/
-make html
+# From crikit/docs directory
+# Build API w/o pyuic5-generated files
+sphinx-apidoc -f -o .\source\ ..\crikit\ ..\crikit\ui\qt_* ..\crikit\ui\*_rc* ..\crikit\ui\old\**
+
+# make html  
+# On Windows
+make.bat html
