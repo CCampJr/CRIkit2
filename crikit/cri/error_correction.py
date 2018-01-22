c185be6c696d2ef176cee8bfe39007f3d2b3957c
Charles H. Camp Jr <ccampjr@gmail.com>
2018-01-22 01:44:23 -0500

    Als now accepts rng, fix_const and made all hyper vectors the length of the full signal regardless of rng

--- crikit/cri/error_correction.py
+++ crikit/cri/error_correction.py
@@ -26,7 +26,8 @@ class PhaseErrCorrectALS:
     """
     def __init__(self, smoothness_param=1, asym_param=1e-2,
                  redux=10, order=2, rng=None, fix_end_points=False,
-                 fix_rng=None, max_iter=100, min_diff=1e-5, **kwargs):
+                 fix_rng=None, fix_const=1, max_iter=100, min_diff=1e-5,
+                 **kwargs):
 
 
         self.rng = _rng_is_pix_vec(rng)
@@ -36,8 +37,10 @@ class PhaseErrCorrectALS:
                         'asym_param' : asym_param,
                         'redux' : redux,
                         'order' : order,
+                        'rng' : rng,
                         'fix_end_points' : fix_end_points,
                         'fix_rng' : fix_rng,
+                        'fix_const' : fix_const,
                         'max_iter' : max_iter,
                         'min_diff' : min_diff})
 
@@ -47,27 +50,37 @@ class PhaseErrCorrectALS:
         self._inst_als = _AlsCvxopt(**kwargs)
 
         try:
+            # if data.ndim>2:
             shp = data.shape[0:-2]
             total_num = _np.array(shp).prod()
-
+            # else:
+            #     shp = ()
+            #     total_num = 1
+            
             counter = 1
             for idx in _np.ndindex(shp):
                 print('Detrended iteration {} / {}'.format(counter, total_num))
                 ph = _np.unwrap(_np.angle(data[idx]))
-                if self.rng is None:
-                    err_phase = self._inst_als.calculate(ph)
-                else:
-                    err_phase = self._inst_als.calculate(ph[..., self.rng])
+                # if self.rng is None:
+                err_phase = self._inst_als.calculate(ph)
+                # else:
+                    # err_phase = self._inst_als.calculate(ph[..., self.rng])
+                # print('Error phase shape: {}'.format(err_phase.shape))
+                # print('Range: {}'.format(self.rng))
+                # raise ValueError
+                h = _np.zeros(err_phase[..., self.rng].shape)
+                h += _hilbert(err_phase[..., self.rng])
 
-                h = _np.zeros(err_phase.shape)
-                h += _hilbert(err_phase)
+                correction_factor = 1/_np.exp(h) * _np.exp(-1j*err_phase[...,self.rng])
 
-                correction_factor = 1/_np.exp(h) * _np.exp(-1j*err_phase)
+                # if self.rng is None:
+                #     ret_obj[idx] *= correction_factor
+                # else:
 
-                if self.rng is None:
-                    ret_obj[idx] *= correction_factor
-                else:
-                    ret_obj[idx][..., self.rng] *= correction_factor
+                # if len(idx) == 0:
+                #     ret_obj[..., self.rng] *= correction_factor
+                # else:
+                ret_obj[idx][..., self.rng] *= correction_factor
                 counter += 1
         except:
             return False
@@ -76,8 +89,12 @@ class PhaseErrCorrectALS:
             return True
 
     def calculate(self, data, **kwargs):
+        if self.rng is None:
+            self.rng = _np.arange(data.shape[-1])
+            
+        data_copy = _np.zeros(data.shape, dtype=data.dtype)
+        data_copy[..., self.rng] = 1*data[..., self.rng]
 
-        data_copy = _copy.deepcopy(data)
         self._k.update(kwargs)
 
         success = self._calc(data, ret_obj=data_copy, **self._k)
@@ -87,6 +104,18 @@ class PhaseErrCorrectALS:
             return None
 
     def transform(self, data, **kwargs):
+        if self.rng is None:
+            self.rng = _np.arange(data.shape[-1])
+
+        total_rng = _np.arange(data.shape[-1])
+
+        not_in_rng = list(set(total_rng).difference(self.rng))
+        not_in_rng.sort()
+        not_in_rng = _np.array(not_in_rng)
+
+        if not_in_rng.size != 0:
+            data[..., not_in_rng] *= 0
+
         self._k.update(kwargs)
 
         success = self._calc(data, ret_obj=data, **self._k)
@@ -128,8 +157,13 @@ class ScaleErrCorrectSG:
             return True
 
     def calculate(self, data):
+        if self.rng is None:
+            self.rng = _np.arange(data.shape[-1])
+            
+        data_copy = _np.zeros(data.shape, dtype=data.dtype)
+        data_copy[..., self.rng] = 1*data[..., self.rng]
 
-        data_copy = _copy.deepcopy(data)
+        # data_copy = _copy.deepcopy(data)
         success = self._calc(data, ret_obj=data_copy)
         if success:
             return data_copy
@@ -137,77 +171,41 @@ class ScaleErrCorrectSG:
             return None
 
     def transform(self, data):
+        if self.rng is None:
+            self.rng = _np.arange(data.shape[-1])
+
+        total_rng = _np.arange(data.shape[-1])
+
+        not_in_rng = list(set(total_rng).difference(self.rng))
+        not_in_rng.sort()
+        not_in_rng = _np.array(not_in_rng)
+
+        if not_in_rng.size != 0:
+            data[..., not_in_rng] *= 0
+
         success = self._calc(data, ret_obj=data)
         return success
 
 
 if __name__ == '__main__':  # pragma: no cover
-    import matplotlib.pyplot as plt
-    from crikit.cri.kk import KramersKronig
-    import timeit
 
-    SPECT_LEN = 878
-    WN = _np.linspace(4000, 500, SPECT_LEN)
-    chi = (1 / ((WN - 1000 - 1j * 10)) +
-           1 / ((WN - 1020 - 1j * 10)) +
-           1 / ((WN - 2800 - 1j * 10)))
-    chiNR = 0*chi + 0.055
-    exc = WN
-    sig = _np.abs(chi + chiNR)**2
+    x = _np.linspace(-100, 100, 1000)
 
-    sigNR = _np.abs(chiNR)**2
-    sigRef = chiNR*(WN/1e3)**.5
+    phi_peak = 10*_np.imag(1/(25-x-1j*2))
+    phi_bg = _np.exp(-x**2/(2*30**2))
+    phi =  phi_peak + phi_bg
+    y = 1*_np.exp(1j*phi)
 
-    NUM_REPS = 10
+    y = _np.dot(_np.ones((100,1)), y[None,:])
+    # y = _np.reshape(y, (10,10,-1))
+    # RSS
+    # assert _np.sum((phi_peak-phi)**2) > 265.0
+    
+    rng = _np.arange(200, 800)
+    # rng = None
+    pec = PhaseErrCorrectALS(smoothness_param=1, asym_param=1e-3, 
+                                redux=10, fix_end_points=True, rng=rng, 
+                                verbose=False)
 
-    kk = KramersKronig()
-    kkd = kk.calculate(sig, sigRef)
-    kkd = _np.dot(_np.random.rand(NUM_REPS,NUM_REPS,1)*_np.ones((NUM_REPS, NUM_REPS, 1)), kkd[None, :])
-
-#    plt.plot(chi.imag/chiNR.real, label='Ideal')
-    plt.plot(kkd[5, 5, :].imag, label='Before Correction')
-
-    start = timeit.default_timer()
-    phase_err_correct_als = PhaseErrCorrectALS(fix_end_points=True,
-                                               smoothness_param=1e8,
-                                               asym_param=1e-3,
-                                               redux=1)
-    success = phase_err_correct_als.transform(kkd, verbose=False)
-    print('Success? : {}'.format(success))
-    stop = timeit.default_timer()
-    print('Sec/spectrum: {:.3g}'.format((stop-start)/NUM_REPS**2))
-
-
-#
-    plt.plot(kkd[5,5,:].imag, label='After PhErr Corr.')
-    plt.legend()
-    plt.show()
-#    scale_err_correct_sg = ScaleErrCorrectSG()
-#    success = scale_err_correct_sg.transform(kkd)
-#    print('Success? : {}'.format(success))
-#    plt.plot(kkd[5, 5, :].imag, label='After Correction')
-#    plt.legend(loc='best')
-#    plt.show()
-
-#    scale_err_correct_sg = ScaleErrCorrectSG()
-#    out = scale_err_correct_sg.calculate(kkd[0,0,:])
-#    plt.plot(out.imag)
-#
-#    scale_err_correct_sg = ScaleErrCorrectSG(win_size=11, order=2)
-#    out = scale_err_correct_sg.calculate(kkd[0,0,:])
-#    plt.plot(out.imag)
-#
-#    plt.show()
-
-#
-#    phase_err_correct_als = PhaseErrCorrectALS(print_iteration=False)
-#    out = phase_err_correct_als.calculate(kkd)
-#
-#    plt.plot(out[0,0,:].imag)
-#
-#    phase_err_correct_als = PhaseErrCorrectALS(smoothness_param=1e1,
-#                                                asym_param=1e-2,
-#                                                redux_factor=1)
-
-
-#    print(phase_err_correct_als._k)
+    y_pec = pec.calculate(y)
+    # print(y_pec[...,:20])
--- crikit/preprocess/algorithms/abstract_als.py
+++ crikit/preprocess/algorithms/abstract_als.py
@@ -10,43 +10,66 @@ from scipy.interpolate import UnivariateSpline as _USpline
 
 class AbstractBaseline:
     
-    def setup(self, redux=1, verbose=False, order=2, fix_end_points=False, 
-              fix_rng=None, max_iter=100, min_diff=1e-5):
+    def setup(self, redux=1, verbose=False, order=2, rng=None, 
+              fix_end_points=False, fix_rng=None, fix_const=1, max_iter=100, min_diff=1e-5):
         self.redux = redux
         self.order = order
+        self.rng = rng
         self.fix_end_points = fix_end_points
-        self.fix_rng = fix_rng
+        self._fix_rng = fix_rng
+
+        self.fix_const = fix_const
         self.max_iter = max_iter
         self.min_diff = min_diff
         
         self.verbose = verbose
         self.t = None
         self.t_per_iter = None
+
+        # Rng applied, then redux
+        self.full_sig_shape = None
+        self.full_sig_spectral_size = None
+        self.n_sig_to_detrend = None
+        self.rng_sig_shape = None
+        self.rng_sig_spectral_size = None
+        self.redux_sig_shape = None
+        self.redux_sig_spectral_size = None
         
     def calculate(self, signal):
-        sig_shape = signal.shape  # Shape of input signal
-        sig_size = signal.shape[-1]  # Length of spectral axis
+        self.full_sig_shape = signal.shape # Shape of input signal
+        self.full_sig_spectral_size = signal.shape[-1]  # Length of spectral axis
+
+        if self.rng is None:
+            self.rng = _np.arange(self.full_sig_spectral_size)
         
+        self.rng_sig_shape = signal[..., self.rng].shape
+        self.rng_sig_spectral_size = self.rng_sig_shape[-1]
+
         # N signals to detrend
-        sig_n_to_detrend = int(signal.size/signal.shape[-1])
+        self.n_sig_to_detrend = int(signal.size/self.full_sig_spectral_size)
         
         tmr = _timeit.default_timer()
         if self.redux == 1:
-            output = self._calc(signal)
+            self.redux_sig_shape = self.rng_sig_shape
+            self.redux_sig_spectral_size = self.redux_sig_shape[-1]
+
+            output = _np.zeros(self.full_sig_shape, dtype=signal.dtype)
+            output[..., self.rng] = self._calc(signal[..., self.rng])
+
         else:  # Sub-sample
             # Dummy indep variable
-            x = _np.arange(sig_size)
+            x = _np.arange(self.rng.size)
             x_sub = _np.linspace(x[0], x[-1], _np.round(x.size / 
                                  self.redux).astype(_np.integer))
+            self.redux_sig_shape = list(self.full_sig_shape)
+            self.redux_sig_shape[-1] = x_sub.size
+            self.redux_sig_spectral_size = self.redux_sig_shape[-1]
 
-            sub_shape = list(sig_shape)
-            sub_shape[-1] = x_sub.size
-            
-            signal_sampled = _np.zeros(sub_shape)
+            signal_sampled = _np.zeros(self.redux_sig_shape)
             
             # Spline interpolation/sub-sampling
-            for coords in _np.ndindex(signal.shape[0:-1]):
-                spl = _USpline(x,signal[coords],s=0)
+            for coords in _np.ndindex(signal.shape[:-1]):
+                spl = _USpline(x,signal[coords][self.rng],s=0)
                 signal_sampled[coords] = spl(x_sub)
             
             # Baseline from sub-sampled signal
@@ -56,13 +79,22 @@ class AbstractBaseline:
             # Spline interpolation/super-sampling
             for coords in _np.ndindex(output_sampled.shape[0:-1]):
                 spl2 = _USpline(x_sub,output_sampled[coords],s=0)
-                output[coords] = spl2(x)
+                output[[*coords, self.rng]] = spl2(x)
             
         tmr -= _timeit.default_timer()
         self.t = -tmr
-        self.t_per_iter = self.t/sig_n_to_detrend
+        self.t_per_iter = self.t/self.n_sig_to_detrend
         
         return output
 
     def _calc(self, signal):
         raise NotImplementedError
+
+    @property
+    def fix_rng(self):
+        if (self.redux == 1) | (self._fix_rng is None):
+            return self._fix_rng
+        else:
+            redux_fix_rng = self._fix_rng / self.redux
+            redux_fix_rng = _np.unique(redux_fix_rng).astype(_np.integer)
+            return redux_fix_rng
