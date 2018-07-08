bb6bc7b530d8891c5118fa7c0830ce3bca85a586
Charles H. Camp Jr <ccampjr@gmail.com>
2018-07-08 11:02:30 -0400

    breadcrumb dict is now OrderedDict

---
+++ crikit/utils/tests/test_breadcrumb.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+import pytest
+
+from crikit.utils.breadcrumb import BCPre
+
+def test_breadcrumb():
+
+    atrdict = BCPre()
+    prefix = BCPre.PREFIX
+
+    with pytest.raises(TypeError):
+        atrdict.add_step('Test1')
+    with pytest.raises(ValueError):
+        atrdict.add_step(['Test',1])
+
+    atrdict.add_step(['Raw'])
+    atrdict.add_step(['SubDark','RangeStart',-1500,'RangeEnd',-400])
+    atrdict.add_step(['NormKK','Amp',100.0,'Phase',10.0])
+
+    assert atrdict.attr_dict[prefix] == 3
+
+    
