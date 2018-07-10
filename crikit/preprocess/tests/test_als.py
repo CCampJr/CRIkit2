85bd0a1b2780f6fd24731586a9b974adbbb8b061
Charles H. Camp Jr <ccampjr@gmail.com>
2018-07-10 09:43:54 -0400

    Added test_rng_redux_fix_rng_vecasym

--- crikit/preprocess/tests/test_als.py
+++ crikit/preprocess/tests/test_als.py
@@ -183,3 +183,25 @@ def test_rng_redux_fix_rng():
     y_als = als.calculate(y)
     assert np.max(y[250:750] - y_als[250:750]) < 0.004
         
+def test_rng_redux_fix_rng_vecasym():
+    x = np.linspace(-100, 100, 1000)
+    y = 10*np.exp(-(x**2/(2*20**2)))
+
+    rng = np.arange(200,800)
+
+    asym_vec = 0*x + 1e-7
+    als = AlsCvxopt(smoothness_param=1, asym_param=asym_vec, rng=rng,
+                    redux=10, fix_end_points=False, fix_rng=None, 
+                    verbose=True)
+    y_als = als.calculate(y)
+
+    assert np.max(y[250:750] - y_als[250:750]) > 9
+
+    fix_rng = np.arange(600)
+    # fix_rng = np.hstack((np.arange(100), np.arange(500,600)))
+
+    als = AlsCvxopt(smoothness_param=1, asym_param=1e-7, rng=rng,
+                    redux=10, fix_end_points=False, fix_rng=fix_rng, 
+                    verbose=True)
+    y_als = als.calculate(y)
+    assert np.max(y[250:750] - y_als[250:750]) < 0.004
