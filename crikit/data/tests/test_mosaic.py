f05333d420f1e479f1c9e4539d0b7bf7f064de8d
Charles H. Camp Jr <ccampjr@gmail.com>
2018-07-16 14:45:19 -0400

    Added tests for a big np dset into sub imgs, then recon by Mosaic.

--- crikit/data/tests/test_mosaic.py
+++ crikit/data/tests/test_mosaic.py
@@ -98,3 +98,91 @@ def test_err_wrong_dim_append():
 
     with pytest.raises(TypeError):
         mos.append(np.random.randn(3,5))
+
+def test_big_to_small_2d():
+    orig_data = np.random.randn(40,5)
+    
+    mos = Mosaic()
+
+    m_unit_size = 10
+    n_unit_size = 5
+
+    m_ct = orig_data.shape[0]//m_unit_size
+    n_ct = orig_data.shape[1]//n_unit_size
+
+    for ct in range(m_ct):
+        mos.append(orig_data[ct*m_unit_size:(ct+1)*m_unit_size,:])
+
+    assert np.allclose(mos.mosaic2d(shape=(m_ct, n_ct), order='R'), orig_data)
+    assert np.allclose(mos.mosaic2d(shape=(m_ct, n_ct), order='C'), orig_data)
+    assert np.allclose(mos.mosaicfull(shape=(m_ct, n_ct), order='R'), orig_data)
+    assert np.allclose(mos.mosaicfull(shape=(m_ct, n_ct), order='C'), orig_data)
+
+def test_big_to_small_2d_2():
+    orig_data = np.random.randn(40,10)
+    
+    mos = Mosaic()
+
+    m_unit_size = 10
+    n_unit_size = 5
+
+    m_ct = orig_data.shape[0]//m_unit_size
+    n_ct = orig_data.shape[1]//n_unit_size
+
+    for ni in range(n_ct):
+        for mi in range(m_ct):
+            mos.append(orig_data[mi*m_unit_size:(mi+1)*m_unit_size,
+                                 ni*n_unit_size:(ni+1)*n_unit_size])
+
+    assert np.allclose(mos.mosaic2d(shape=(m_ct, n_ct), order='R'), orig_data)
+    assert not np.allclose(mos.mosaic2d(shape=(m_ct, n_ct), order='C'), orig_data)
+    assert np.allclose(mos.mosaicfull(shape=(m_ct, n_ct), order='R'), orig_data)
+    assert not np.allclose(mos.mosaicfull(shape=(m_ct, n_ct), order='C'), orig_data)
+
+def test_big_to_small_3d():
+    orig_data = np.random.randn(40,5,3)
+    
+    mos = Mosaic()
+
+    m_unit_size = 10
+    n_unit_size = 5
+
+    m_ct = orig_data.shape[0]//m_unit_size
+    n_ct = orig_data.shape[1]//n_unit_size
+
+    for ct in range(m_ct):
+        mos.append(orig_data[ct*m_unit_size:(ct+1)*m_unit_size, ...])
+
+    assert np.allclose(mos.mosaic2d(shape=(m_ct, n_ct), order='R', idx=0), orig_data[...,0])
+    assert np.allclose(mos.mosaic2d(shape=(m_ct, n_ct), order='C', idx=0), orig_data[...,0])
+    assert np.allclose(mos.mosaic2d(shape=(m_ct, n_ct), order='R', idx=1), orig_data[...,1])
+    assert np.allclose(mos.mosaic2d(shape=(m_ct, n_ct), order='C', idx=1), orig_data[...,1])
+    assert np.allclose(mos.mosaic2d(shape=(m_ct, n_ct), order='R', idx=2), orig_data[...,2])
+    assert np.allclose(mos.mosaic2d(shape=(m_ct, n_ct), order='C', idx=2), orig_data[...,2])
+    assert np.allclose(mos.mosaicfull(shape=(m_ct, n_ct), order='R'), orig_data)
+    assert np.allclose(mos.mosaicfull(shape=(m_ct, n_ct), order='C'), orig_data)
+
+def test_big_to_small_3d_2():
+    orig_data = np.random.randn(40,10, 3)
+    
+    mos = Mosaic()
+
+    m_unit_size = 10
+    n_unit_size = 5
+
+    m_ct = orig_data.shape[0]//m_unit_size
+    n_ct = orig_data.shape[1]//n_unit_size
+
+    for ni in range(n_ct):
+        for mi in range(m_ct):
+            mos.append(orig_data[mi*m_unit_size:(mi+1)*m_unit_size,
+                                 ni*n_unit_size:(ni+1)*n_unit_size, :])
+
+    assert np.allclose(mos.mosaic2d(shape=(m_ct, n_ct), idx=0, order='R'), orig_data[..., 0])
+    assert not np.allclose(mos.mosaic2d(shape=(m_ct, n_ct), idx=0, order='C'), orig_data[..., 0])
+    assert np.allclose(mos.mosaic2d(shape=(m_ct, n_ct), idx=1, order='R'), orig_data[..., 1])
+    assert not np.allclose(mos.mosaic2d(shape=(m_ct, n_ct), idx=1, order='C'), orig_data[..., 1])
+    assert np.allclose(mos.mosaic2d(shape=(m_ct, n_ct), idx=2, order='R'), orig_data[..., 2])
+    assert not np.allclose(mos.mosaic2d(shape=(m_ct, n_ct), idx=2, order='C'), orig_data[..., 2])
+    assert np.allclose(mos.mosaicfull(shape=(m_ct, n_ct), order='R'), orig_data)
+    assert not np.allclose(mos.mosaicfull(shape=(m_ct, n_ct), order='C'), orig_data)
