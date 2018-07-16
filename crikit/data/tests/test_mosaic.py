898bbf4e3a941666f94ebd2194ce58eae11d20fb
Charles H. Camp Jr <ccampjr@gmail.com>
2018-07-16 15:13:33 -0400

    Added test for 3D when output dset is given for inplace writing

--- crikit/data/tests/test_mosaic.py
+++ crikit/data/tests/test_mosaic.py
@@ -202,3 +202,43 @@ def test_big_to_small_3d_2():
     assert not np.allclose(mos.mosaic2d(shape=(m_ct, n_ct), idx=2, order='C'), orig_data[..., 2])
     assert np.allclose(mos.mosaicfull(shape=(m_ct, n_ct), order='R'), orig_data)
     assert not np.allclose(mos.mosaicfull(shape=(m_ct, n_ct), order='C'), orig_data)
+
+
+def test_big_to_small_3d_output_given():
+    orig_data = np.random.randn(40,10, 3)
+    
+    mos = Mosaic()
+
+    m_unit_size = 10
+    n_unit_size = 5
+
+    m_ct = orig_data.shape[0]//m_unit_size
+    n_ct = orig_data.shape[1]//n_unit_size
+
+    for ni in range(n_ct):
+        for mi in range(m_ct):
+            mos.append(orig_data[mi*m_unit_size:(mi+1)*m_unit_size,
+                                 ni*n_unit_size:(ni+1)*n_unit_size, :])
+
+    output_data = np.zeros(orig_data.shape[:-1], dtype=orig_data.dtype)
+    id_in = id(output_data)
+    mos.mosaic2d(shape=(m_ct, n_ct), idx=0, order='R', out=output_data)
+    id_out = id(output_data)
+    assert np.allclose(output_data, orig_data[..., 0])
+    assert id_in == id_out
+
+    output_data = np.zeros(orig_data.shape[:-1], dtype=orig_data.dtype)
+    mos.mosaic2d(shape=(m_ct, n_ct), idx=1, order='R', out=output_data)
+    assert np.allclose(output_data, orig_data[..., 1])
+
+    output_data = np.zeros(orig_data.shape[:-1], dtype=orig_data.dtype)
+    mos.mosaic2d(shape=(m_ct, n_ct), idx=2, order='R', out=output_data)
+    assert np.allclose(output_data, orig_data[..., 2])
+
+    output_data = np.zeros(orig_data.shape, dtype=orig_data.dtype)
+    mos.mosaicfull(shape=(m_ct, n_ct), order='R', out=output_data)
+    assert np.allclose(output_data, orig_data)
+
+    output_data = np.zeros(orig_data.shape, dtype=orig_data.dtype)
+    mos.mosaicfull(shape=(m_ct, n_ct), order='C', out=output_data)
+    assert not np.allclose(output_data, orig_data)
