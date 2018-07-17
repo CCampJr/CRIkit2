a6f43a39a558526a069c4f391582e4ee1dd6cb3c
Charles H. Camp Jr <ccampjr@gmail.com>
2018-07-17 12:42:50 -0400

    Added in a 3D crop test

--- crikit/data/tests/test_mosaic_hdf.py
+++ crikit/data/tests/test_mosaic_hdf.py
@@ -104,3 +104,73 @@ def test_big_to_small_3d_output_given():
         os.remove(filename_out)
     except:
         print('Could not delete {}'.format(filename_out))
+
+def test_big_to_small_3d_output_given_crop():
+    """ 3D big dataset, divied up into small chunks -- WITH CROPPING """
+    orig_data_np = np.random.randn(40, 10, 3)
+
+    filename_in = 'test_h5mosaic_in.h5'
+    dset_in_prefix = 'dset_'
+    fid_in = h5py.File(filename_in, 'w')
+
+    m_unit_size = 10
+    n_unit_size = 5
+
+    m_ct = orig_data_np.shape[0]//m_unit_size
+    n_ct = orig_data_np.shape[1]//n_unit_size
+
+    ct = 0
+    for ni in range(n_ct):
+        for mi in range(m_ct):
+            temp = orig_data_np[mi*m_unit_size:(mi+1)*m_unit_size,
+                                ni*n_unit_size:(ni+1)*n_unit_size, :]
+            fid_in.create_dataset(dset_in_prefix+'{}'.format(ct), shape=temp.shape, data=temp)
+            ct += 1
+
+    fid_in.close()
+
+    fid_in = h5py.File(filename_in, 'r')
+
+    mos = Mosaic()
+    mos.parameters['StartR'] = 1
+    mos.parameters['EndR'] = -1
+    mos.parameters['StartC'] = 1
+    mos.parameters['EndC'] = -1
+
+    ct = 0
+    for ni in range(n_ct):
+        for mi in range(m_ct):
+            mos.append(fid_in[dset_in_prefix+'{}'.format(ct)])
+            ct += 1
+
+    filename_out = 'test_h5mosaic_out.h5'
+    dset_out_name = 'dset'
+
+    fid_out = h5py.File(filename_out, 'w')
+    fid_out.create_dataset(dset_out_name, shape=mos.mosaic_shape((m_ct, n_ct)),
+                           dtype=orig_data_np.dtype)
+
+    mos.mosaicfull((m_ct, n_ct), out=fid_out[dset_out_name], order='R')
+
+    assert np.allclose(fid_out[dset_out_name][0:3,0:3,:],
+                       orig_data_np[1:4, 1:4, :])
+    assert np.allclose(fid_out[dset_out_name][8:11, 0:3, :],
+                       orig_data_np[11:14, 1:4, :])
+
+    assert np.allclose(fid_out[dset_out_name][0:3, 8:11, :],
+                       orig_data_np[1:4, 11:14, :])
+
+    fid_in.close()
+    fid_out.close()
+
+    time.sleep(1)
+    try:
+        os.remove(filename_in)
+    except:
+        print('Could not delete {}'.format(filename_in))
+
+    time.sleep(1)
+    try:
+        os.remove(filename_out)
+    except:
+        print('Could not delete {}'.format(filename_out))
