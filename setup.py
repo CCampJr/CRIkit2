20a645558656d8b83cf2221379a284ae549e0c78
Camp <chc@NIST.GOV>
2019-05-23 11:37:51 -0400

    Added Py37 to setup [ci skip]

--- setup.py
+++ setup.py
@@ -35,4 +35,5 @@ setup(name='crikit2',
                    'Programming Language :: Python :: 3.4',
                    'Programming Language :: Python :: 3.5',
                    'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
                    'Topic :: Scientific/Engineering :: Visualization'])
